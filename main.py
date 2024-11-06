2dfe3987942fccb57485213c87570a55e34945e4
CooperNazar <ccnazar@loyola.edu>
2024-11-05 20:37:49 -0500

    Comments added

--- main.py
+++ main.py
@@ -7,10 +7,6 @@
 # Data Out: List of the sum of the dice when rolled and chart of the distribution of rolls
 
 
-# PLACEHOLDER FOR PRINTING LIST
-# print(list[2]*'*')
-
-
 import random
 
 sum_list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -22,7 +18,7 @@ sum = 0
 # Return: Number user inputs as an integer
 def roll_amount():
     num_roll = input("How many times do you want to roll the dice? ")
-    while not num_roll.isdigit(): #to make sure the user enters a valid value
+    while not num_roll.isdigit(): # To make sure the user enters a valid value
         print("Please enter a valid number.")
         num_roll = input("How many times do you want to roll the dice? ")
     num_roll = int(num_roll)
@@ -36,7 +32,7 @@ def roll_amount():
 def roll_dice():
     roll1 = random.randint(1,6)
     roll2 = random.randint(1,6)
-    sum = roll1 + roll2
+    sum = roll1 + roll2 # Add the results of the two dice rolls
     return sum
 
 
@@ -44,7 +40,7 @@ def roll_dice():
 # Parameters: Sum
 # Return: None
 def add_to_list(sum):
-    sum_list[sum] = sum_list[sum] + 1
+    sum_list[sum] = sum_list[sum] + 1 # Adds a mark to the sum that is associated with the sum
 
 
 # Purpose: Print the list
@@ -55,21 +51,21 @@ def print_list():
     count_list = 0
     total = 10
     while count_list <= total:
-        print(f'Sum of {count_list + 2:02}: {sum_list[count_list + 2]*'*'}')
+        print(f'Sum of {count_list + 2:02}: {sum_list[count_list + 2]*'*'}') # Prints the sum number and a mark for each time it was rolled
         count_list += 1
 
 
-# Purpose:
+# Purpose: Give the purpose, run the program, and print the result
 # Parameters: None
 # Return: None
 def main():
     print('This program displays the distribution of rolls from two dice')
-    num_roll = roll_amount()
+    num_roll = roll_amount() # Calls the roll_amount function to get the number of times to roll the dice
     count = 0
     while count < num_roll:
         sum = roll_dice()
-        add_to_list(sum)
+        add_to_list(sum) # Adds one space in the spot of the sum in the list
         count += 1
-    print_list()
+    print_list() # Prints the list
 
 main()
